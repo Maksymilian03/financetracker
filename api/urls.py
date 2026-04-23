1e1bcde0836f7c9cf0348d0d311caaf5f40e2fb4
Maksymilian03 <maksiu2003xd@gmail.com>
2026-04-23 19:20:33 +0200

    Add Transaction and Investment viewsets and endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import CategoryViewSet
+from .views import CategoryViewSet, InvestmentViewSet, TransactionViewSet
 
 router = DefaultRouter()
 router.register('category', CategoryViewSet, basename='category')
+router.register('transaction', TransactionViewSet, basename='transaction')
+router.register('investment', InvestmentViewSet, basename='investment')
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
-from .serializers import CategorySerializer
-from .models import Category
+from .serializers import CategorySerializer, TransactionSerializer, InvestmentSerializer
+from .models import Category, Transaction, Investment
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -11,3 +11,24 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class InvestmentViewSet(viewsets.ModelViewSet):
+    queryset = Investment.objects.all()
+    serializer_class = InvestmentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+
+
+
+
